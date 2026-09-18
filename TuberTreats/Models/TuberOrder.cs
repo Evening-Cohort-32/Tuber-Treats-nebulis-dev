51ff7467ff9b7675c5646fc96f84acae5468e108
Jaiden Rothrock <jaiden_rothrock@hotmail.com>
2026-09-18 17:09:44 -0500

    Added Entitry Models

--- TuberTreats/Models/TuberOrder.cs
+++ TuberTreats/Models/TuberOrder.cs
@@ -1,1 +1,13 @@
 namespace TuberTreats.Models;
+
+public class TuberOrder
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public int TuberDriverId { get; set; }
+
+    public DateTime OrderPlacedOnDate { get; set; }
+    public DateTime? DeliveredOnDate { get; set; }
+
+    public List<TuberTopping> Toppings { get; set; }
+}
